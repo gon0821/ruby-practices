ead10dd07d484a7b44159048de9144996b0b485c
gon821 <gonty821@gmail.com>
2025-01-28 21:39:26 +0900

    feature: add fizzbuzz method

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,17 @@
+#!/bin/sh ruby
+
+def fizzbuzz(min, max)
+  (min..max).each do |i|
+    if i % 3 == 0 && i % 5 == 0
+      puts "FizzBuzz"
+    elsif i % 3 == 0
+      puts "Fizz"
+    elsif i % 5 == 0
+      puts "Buzz"
+    else
+      puts i
+    end
+  end
+end
+
+fizzbuzz(1, 20)
