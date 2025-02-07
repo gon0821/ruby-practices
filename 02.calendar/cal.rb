b2c5ccf71b4d3ab8e21b1f34c589ef337faedb9d
gon821 <gonty821@gmail.com>
2025-02-08 01:20:19 +0900

    refactoring after 1st review

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,54 +2,37 @@
 require 'date'
 require 'optparse'
 
-INDENT = [3, 6, 9, 12, 15, 18, 21].freeze
-DOW = %w(日 月 火 水 木 金 土).freeze
+INDENT = (3..21).step(3).to_a.freeze
+DAY_OF_WEEK = %w(日 月 火 水 木 金 土).freeze
 
 params = ARGV.getopts('y:','m:')
-year = params['y'] ? params['y'].to_i : Date.today.year
-month = params['m'] ? params['m'].to_i : Date.today.month
+year = params['y']&.to_i || Date.today.year
+month = params['m']&.to_i || Date.today.month
 
 def set_year_month(year, month)
   puts "#{month}月　#{year}".rjust(15)
 end
 
 def set_day_of_the_week
-  DOW.each_with_index do |dow, index|
-    if index == 6
-      puts "#{dow}".rjust(2)
-    else
-      print "#{dow}".rjust(2)
-    end
+  DAY_OF_WEEK.each do |day|
+    print "#{day}".rjust(2)
   end
+  print "\n"
 end
 
 def display_dates(year, month)
   beginning_of_month = Date.new(year, month, 1)
   end_of_month = Date.new(year, month, -1)
 
-  (beginning_of_month..end_of_month).each_with_index do |day, index|
-    if index == 0
-      set_beginning_of_month(day)
-    else
-      set_dates_of_month(day)
-    end
+  set_dates_of_month(beginning_of_month, INDENT[beginning_of_month.wday])
+
+  (beginning_of_month.next..end_of_month).each do |day|
+    set_dates_of_month(day, INDENT[0])
   end
 end
 
-def set_beginning_of_month(day)
-  if day.saturday?
-    puts "#{day.day}".rjust(INDENT[day.wday])
-  else
-    print "#{day.day}".rjust(INDENT[day.wday])
-  end
-end
-
-def set_dates_of_month(day)
-  if day.saturday?
-    puts "#{day.day}".rjust(3)
-  else
-    print "#{day.day}".rjust(3)
-  end
+def set_dates_of_month(day, indent)
+  print day.saturday? ? "#{day.day}\n".rjust(indent + 1) : "#{day.day}".rjust(indent)
 end
 
 set_year_month(year, month)
