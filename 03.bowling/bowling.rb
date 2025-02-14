aa53f2c8a3c67d30caabf30d71168c35ba33a0c0
gon821 <gonty821@gmail.com>
2025-02-14 21:55:23 +0900

    create bowling score program

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,42 +1,31 @@
 #!/usr/bin/env ruby
-require 'debug'
 
 params = ARGV[0]
 scores = params.split(',')
-# ["8", "2", "9", "0", "7", "2", "10", "8", "2", "6", "2", "7", "3", "8", "0", "9", "1"]
 
 shots = []
 scores.each do |score|
-  if score == "10"
+  if score == "X"
     shots << 10
     shots << 0
   else
     shots << score.to_i
   end
 end
-# [8, 2, 9, 0, 7, 2, 10, 0, 8, 2, 6, 2, 7, 3, 8, 0, 9, 1]
 
 frames = shots.each_slice(2).to_a
-# [[8, 2], [9, 0], [7, 2], [10, 0], [8, 2], [6, 2], [7, 3], [8, 0], [9, 1]]
-# [[7, 1], [10, 0], [10, 0], [2, 5], [6, 0], [8, 2], [10, 0], [1, 2], [7, 1]]
 
 point = 0
-spare = false
-frames.each do |frame|
-  if frame.sum == 10
-    if spare
-      point += frame.sum + frame[0]
-    else
-      point += frame.sum
-    end
-    spare = true
+
+frames.each_with_index do |frame, i|
+  if frame[0] == 10
+    point += frame.sum + frames[i+1][0] + (frames[i+1][0] == 10 ? frames[i+2][0] : frames[i+1][1])
+  elsif frame.sum == 10
+    point += frame.sum + frames[i+1][0]
   else
-    if spare
-      point += frame.sum + frame[0]
-    else
-      point += frame.sum
-    end
-    spare = false
+    point += frame.sum
   end
+  break if i == 9
 end
+
 p point
