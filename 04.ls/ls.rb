3545406e3648cf05f98c859fd20281de9350c824
gon821 <gonty821@gmail.com>
2025-08-12 23:20:43 +0900

    update after 2nd FB

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,26 +2,26 @@
 
 COLUMN_SIZE = 3
 
-directory_contents = Dir.glob('*')
+entries = Dir.glob('*')
 
-row_size = directory_contents.size.fdiv(COLUMN_SIZE).ceil
+row_size = entries.size.ceildiv(COLUMN_SIZE)
 
-def sliced_directory_contents(directory_contents, row_size)
-  directory_contents.each_slice(row_size).to_a
+def slice_entries(entries, row_size)
+  entries.each_slice(row_size).to_a
 end
 
-def transposed_directory_contents(directory_contents, row_size)
+def transpose_entries(entries, row_size)
   Array.new(row_size) do |index|
-    directory_contents.map do |row_content|
+    entries.map do |row_content|
       row_content[index]
     end
   end
 end
 
-sliced_directory_contents = sliced_directory_contents(directory_contents, row_size)
-formatted_directory_contents = transposed_directory_contents(sliced_directory_contents, row_size)
+slice_entries = slice_entries(entries, row_size)
+formatted_entries = transpose_entries(slice_entries, row_size)
 
-formatted_directory_contents.each do |row|
+formatted_entries.each do |row|
   row.each do |column|
     print column&.ljust(24)
   end
