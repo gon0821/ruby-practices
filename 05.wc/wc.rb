de7399398cb3716f86e31708982e3e3e1d21d7da
gon821 <gonty821@gmail.com>
2026-02-27 07:29:40 +0900

    total表示の実装

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -1,15 +1,31 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def file_detail(file)
+def file_result(file)
   [
-    File.read(file).scan(/\R/).count.to_s.rjust(8),
-    File.read(file).split.count.to_s.rjust(7),
-    File.read(file).bytesize.to_s.rjust(7),
-    file
-  ].join(' ')
+    File.read(file).scan(/\R/).count,
+    File.read(file).split.count,
+    File.read(file).bytesize,
+  ]
 end
 
+def format_result(result, name)
+  result[0] = result[0].to_s.rjust(8)
+  result[1] = result[1].to_s.rjust(7)
+  result[2] = result[2].to_s.rjust(7)
+  result[3] = name
+  result.join(' ')
+end
+
+sum = [0, 0, 0]
 ARGV.each do |file|
-  puts file_detail(file)
+  result = file_result(file)
+  sum[0] += result[0]
+  sum[1] += result[1]
+  sum[2] += result[2]
+  puts format_result(result, file)
+end
+
+if ARGV.count >= 2
+  puts format_result(sum, 'total')
 end
